e8b913119d033236ff8165ecc335eca1bd093129
minhnp2 <minhnp2@illinois.edu>
2022-09-18 18:40:41 -0500

    Added requirements.txt

---
+++ src/backend/login.py
@@ -0,0 +1,15 @@
+class Login:
+    def __init__(self) -> None:
+        pass
+    def register(self) -> None:
+        pass
+    def login(self, user: str, password: str) -> bool:
+        pass
+    def logout(self) -> None:
+        pass
+    def is_authenticated(self) -> bool:
+        pass
+    def get_id(self) -> str:
+        pass
+    def get_name(self) -> str:
+        pass
