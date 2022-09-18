e9c4db79f2a9c52223c1e3e613a23045ce37b4d3
zfeng13 <zfeng13@illinois.edu>
2022-09-18 13:52:16 -0500

    test connection to dbs

--- src/backend/init_db.py
+++ src/backend/init_db.py
@@ -1,10 +1,15 @@
 import sqlite3
 
-connection = sqlite3.connect('database.db')
-
-
+connection = sqlite3.connect('database.db') 
 with open('schema.sql') as f:
     connection.executescript(f.read())
-
+cur = connection.cursor()
+# test insertion
+cur.execute("INSERT INTO Users (username, password, email, phone, apt_id) VALUES (?, ?, ?, ?, ?)",
+            ('XXXXX', 'XXXXXXXXX', 'XXXX@XXX.XXX', '(XXX)XXX-XXX', 1)
+            )
+# test selection
+res = connection.execute('SELECT * FROM Users').fetchall()
+print(res)
 connection.commit()
 connection.close()
