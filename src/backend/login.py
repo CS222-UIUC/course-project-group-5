b1e39d771881f7a935669161159a01d559ab6398
Aden Krakman <akrakman@gmail.com>
2022-09-28 14:25:42 -0500

    created setup, delete register, and test login functions

--- src/backend/login.py
+++ src/backend/login.py
@@ -9,14 +9,16 @@ class Login:
         self.connection = sqlite3.connect("database.db")
         self.cursor = self.connection.cursor()
     def register(self, username: str, email: str, password: str, phone: str) -> bool:
-        '''Register function, returns false if username is take'''
+        '''Register function, returns false if username is taken'''
+        if "@" not in email:
+            return True
         check = self.cursor.execute("SELECT username FROM Users WHERE username = ?", (username, )).fetchall()
-        if not check:
+        if not check: # valid
             self.cursor.execute(
             "INSERT INTO Users (username, email, password, phone) VALUES (?, ?, ?, ?)",
             (username, email, password, phone))
-            return False
-        return True
+            return True
+        return False
     def delete_register_for_tests(self, username: str) -> str:
         self.cursor.execute(
         "DELETE FROM Users WHERE username = ?", (username, ))
--- src/backend/test_backend.py
+++ src/backend/test_backend.py
@@ -6,17 +6,34 @@ import sqlite3
 class TestLogin:
     first_login = Login()
     username = "sadf23"
+    password1 = "passw"
+    password2 = "dsc"
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+    def register_setup(self) -> bool:
+        register = self.first_login.register(self.username, "akfda@gmail.com", "passwor", "213-342-123")
+        return register
+    def delete_register(self) -> str:
+        delete = self.first_login.delete_register_for_tests(self.username)
+        return delete
     def test_register(self):
         self.test_register_helper()
         # do it again to confirm
         self.test_register_helper()
     def test_register_helper(self): # I would pass in the username but it's not working
-        first_register_bool = self.first_login.register(self.username, "akfda@gmail.com", "passwor", "213-342-123")
-        second_register_bool = self.first_login.register(self.username, "akfda@gmail.com", "passwor", "213-342-123")
-        assert first_register_bool == False
-        assert second_register_bool == True
-        delete = self.first_login.delete_register_for_tests(self.username)
+        first_register_bool = self.register_setup()
+        second_register_bool = self.register_setup()
+        assert first_register_bool == True
+        assert second_register_bool == False
+        delete = self.delete_register()
         assert delete == self.username
     def test_login(self):
+        self.register_setup()
+        user = self.first_login.login(1, "passw")
+        check = self.cursor.execute(
+            "SELECT username, email, password FROM Users WHERE (username = ? OR email = ?) \
+            AND password = ?",
+            (1, 1, "passw")).fetchall()
+        assert user == check
         pass
         
