7633039b0d819f56f831669dd3d6219dc8d5baac
minhnp2 <minhnp2@illinois.edu>
2022-10-09 20:53:12 -0500

    Changed apt.py and review.py to actual dataclasses

--- src/backend/apt.py
+++ src/backend/apt.py
@@ -1,36 +1,14 @@
 """Contains Apartment class"""
-from typing import Tuple
+from dataclasses import dataclass
 
 
+@dataclass
 class Apt:
     """Apt class, stores detail about an apartment"""
 
-    def __init__(
-        self, name: str, address: str, rating: int, price: Tuple[int, int]
-    ) -> None:
-        """Constructor"""
-        # TODO: Implement in week 3
-        #       The first argument of the tuple is the min price
-        #       The second argument is the max price
-        #       Have to group them in a tuple to satisfy the pylint god
-        #       Formula for rating: total_upvote - total_downvote
-
-    def get_name(self) -> str:
-        """Returns name of apartment"""
-        # TODO: Implement in week 3
-
-    def get_address(self) -> str:
-        """Returns address of apartment"""
-        # TODO: Implement in week 3
-
-    def get_rating(self) -> int:
-        """Returns rating of apartment"""
-        # TODO: Implement in week 3
-
-    def get_price_max(self) -> int:
-        """Returns maximum price of apartment"""
-        # TODO: Implement in week 3
-
-    def get_price_min(self) -> int:
-        """Returns minimum price of apartment"""
-        # TODO: Implement in week 3
+    apt_id: int
+    name: str
+    address: str
+    rating: int
+    price_min: int
+    price_max: int
--- src/backend/review.py
+++ src/backend/review.py
@@ -1,25 +1,13 @@
 """Contains Review class"""
+from dataclasses import dataclass
 
 
+@dataclass
 class Review:
     """Rating class, stores details about a particular user review"""
 
-    def __init__(self, username: str, date: str, comment: str, vote: bool) -> None:
-        """Constructor"""
-        # TODO: Implement in week 3
-
-    def get_date(self) -> int:
-        """Returns date review was posted"""
-        # TODO: Implement in week 3
-
-    def get_username(self) -> str:
-        """Returns username of the person posting review"""
-        # TODO: Implement in week 3
-
-    def get_comment(self) -> str:
-        """Returns comment posted with the review"""
-        # TODO: Implement in week 3
-
-    def get_vote(self) -> bool:
-        """Returns boolean indicating upvote/downvote"""
-        # TODO: Implement in week 3
+    review_id: int
+    username: str
+    date: str
+    comment: str
+    vote: bool
