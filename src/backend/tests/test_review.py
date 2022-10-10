e9d44ca6b6a8a0dba6dae5b4cafeeff05f90cfd1
minhnp2 <minhnp2@illinois.edu>
2022-10-09 21:02:35 -0500

    Changed test_review.py to reflect dataclass

--- src/backend/tests/test_review.py
+++ src/backend/tests/test_review.py
@@ -5,24 +5,27 @@ from review import Review
 class TestReview:
     """Test Review class"""
 
+    sample_id = 0
     sample_username = "Minh Phan"
     sample_date = "2022-10-07"
     sample_comment = "Decent hall"
     sample_vote = True
-    sample_review = Review(sample_username, sample_date, sample_comment, sample_vote)
+    sample_review = Review(
+        sample_id, sample_username, sample_date, sample_comment, sample_vote
+    )
 
     def test_username(self):
         """Test class stores correct username"""
-        assert self.sample_username == self.sample_review.get_username()
+        assert self.sample_username == self.sample_review.username
 
     def test_date(self):
         """Test class stores correct date"""
-        assert self.sample_date == self.sample_review.get_date()
+        assert self.sample_date == self.sample_review.date
 
     def test_comment(self):
         """Test class stores correct comment"""
-        assert self.sample_comment == self.sample_review.get_comment()
+        assert self.sample_comment == self.sample_review.comment
 
     def test_vote(self):
         """Test class stores correct vote"""
-        assert self.sample_vote == self.sample_review.get_vote()
+        assert self.sample_vote == self.sample_review.vote
