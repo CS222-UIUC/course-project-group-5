d19fe51f5003edbaa587e61feeca0f65bf8b6f6e
Samuel Du <samueldu113@gmail.com>
2022-10-09 21:10:23 -0500

    configured eslint and prettier

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import './App.css';
-import Login from './components/Login'
-import Header from './components/Header' 
+import Header from './components/Header';
 import Register from './components/Register';
 
 function App() {
-
-  return (
-    <div className="App">
-      <Header/>
-      {/* need to add routing to switch between register/login */}
-      <Register/> 
-    </div>
-  );
+   return (
+      <div className="App">
+         <Header />
+         {/* need to add routing to switch between register/login */}
+         <Register />
+      </div>
+   );
 }
 
 export default App;
--- src/frontend/src/components/Login.tsx
+++ src/frontend/src/components/Login.tsx
@@ -1,70 +1,101 @@
 import React from 'react';
-import { Grid, Paper, TextField, Button, Typography, Link, FormControlLabel, Checkbox } from '@mui/material';
+import {
+   Grid,
+   Paper,
+   TextField,
+   Button,
+   Typography,
+   Link,
+   FormControlLabel,
+   Checkbox,
+} from '@mui/material';
 import PersonIcon from '@mui/icons-material/Person';
-import axios from "axios"
+import axios from 'axios';
 import { useState } from 'react';
 
-function sendData(username:string, email:string, password:string) {
-  axios({
+function sendData(username: string, email: string, password: string) {
+   axios({
       method: 'post',
       url: '/login',
       data: {
-          username:username,
-          email:email,
-          password:password,
+         username: username,
+         email: email,
+         password: password,
       },
-      
-  }).then((response) => {
-      console.log(response)
-  }).catch((error) => {
-      if(error.response) {
-          console.log(error.response)
-          console.log(error.response.status)
-          console.log(error.response.headers)
-      }
-  });
+   })
+      .then((response) => {
+         console.log(response);
+      })
+      .catch((error) => {
+         if (error.response) {
+            console.log(error.response);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+         }
+      });
 }
 
 export default function Login() {
+   const [username, setUsername] = useState('');
+   const [email, setEmail] = useState('');
+   const [password, setPassword] = useState('');
 
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const paperStyle = { padding: 20, height: '55vh', width: 310, margin: "20px auto" };
-  const btnstyle = { margin: '8px 0' };
-  return (
-    <Grid>
-      <Paper elevation={12} style={paperStyle}>
-        <PersonIcon fontSize="large" />
-        <h2>Sign In</h2>
-        <TextField label='Username' placeholder='Enter Username' onChange={(event)=>setUsername(event.target.value)} fullWidth required />
-        <TextField label='Email' placeholder='Enter Email' onChange={(event)=>setEmail(event.target.value)} fullWidth required />
-        <TextField label='Password' placeholder='Enter Password' type='password' onChange={(event)=>setPassword(event.target.value)} fullWidth required />
-
-        <FormControlLabel
-          control={
-            <Checkbox
-              name="checkedB"
-              color="primary"
+   const paperStyle = {
+      padding: 20,
+      height: '55vh',
+      width: 310,
+      margin: '20px auto',
+   };
+   const btnstyle = { margin: '8px 0' };
+   return (
+      <Grid>
+         <Paper elevation={12} style={paperStyle}>
+            <PersonIcon fontSize="large" />
+            <h2>Sign In</h2>
+            <TextField
+               label="Username"
+               placeholder="Enter Username"
+               onChange={(event) => setUsername(event.target.value)}
+               fullWidth
+               required
             />
-          }
-          label="Remember me"
-        />
-        <Button type='submit' color="primary" variant="contained" style={btnstyle} onClick = {() => sendData(username, email, password)} fullWidth>Sign in</Button>
-        <Typography >
-          <Link href="#" >
-            Forgot Password
-          </Link>
-        </Typography>
-        <Typography >
-          <Link href="#" >
-            Sign Up
-          </Link>
-        </Typography>
-      </Paper>
-    </Grid>
-  );
+            <TextField
+               label="Email"
+               placeholder="Enter Email"
+               onChange={(event) => setEmail(event.target.value)}
+               fullWidth
+               required
+            />
+            <TextField
+               label="Password"
+               placeholder="Enter Password"
+               type="password"
+               onChange={(event) => setPassword(event.target.value)}
+               fullWidth
+               required
+            />
+
+            <FormControlLabel
+               control={<Checkbox name="checkedB" color="primary" />}
+               label="Remember me"
+            />
+            <Button
+               type="submit"
+               color="primary"
+               variant="contained"
+               style={btnstyle}
+               onClick={() => sendData(username, email, password)}
+               fullWidth
+            >
+               Sign in
+            </Button>
+            <Typography>
+               <Link href="#">Forgot Password</Link>
+            </Typography>
+            <Typography>
+               <Link href="#">Sign Up</Link>
+            </Typography>
+         </Paper>
+      </Grid>
+   );
 }
-
-
