908b3a3659fde6e0bbda4f6ee655b4afb7962af9
minhnp2 <minhnp2@illinois.edu>
2022-10-18 14:15:45 -0500

    Fix pylint error in init_db.py

--- src/backend/database/init_db.py
+++ src/backend/database/init_db.py
@@ -2,7 +2,7 @@
 import sqlite3
 
 connection = sqlite3.connect("database.db")
-with open("schema.sql", "r") as f:
+with open("schema.sql", "r", encoding="utf-8") as f:
     connection.executescript(f.read())
 cur = connection.cursor()
 # test insertion
