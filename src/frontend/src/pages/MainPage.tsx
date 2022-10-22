d6875957c7774c3f5c386a9f199e4b361f5d932e
Aden Krakman <akrakman@gmail.com>
2022-10-22 13:10:29 -0500

    Creates search bar and left section (#24)
    * added scrolling, card, and searchbar component

---
+++ src/frontend/src/pages/MainPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Searching from '../components/Searching';
+import LeftSection from '../sections/MainPageLeftSection';
+
+function MainPage() {
+   return (
+      <div>
+         <Searching />
+         <LeftSection />
+      </div>
+   );
+}
+
+export default MainPage;
---
+++ src/frontend/src/sections/MainPageLeftSection.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Searching from '../components/Searching';
+
+export default function LeftSection(props: any) {
+   return (
+      <div style={{ marginTop: '200px' }}>
+         <Searching />
+      </div>
+   );
+}
