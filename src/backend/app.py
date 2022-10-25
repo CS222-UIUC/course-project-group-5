4c4244920df14f1413a52cd8cf4a906915c0f809
Aden Krakman <akrakman@gmail.com>
2022-10-25 14:49:38 -0500

    add back files

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,13 +1,10 @@
 """ This is a module docstring """
 from flask import Flask, request
-from flask_cors import CORS
 from login import Login
 
 # from logging import FileHandler, WARNING
 
 app = Flask(__name__)
-
-CORS(app)
 
 
 @app.route("/login", methods=["POST", "GET"])
@@ -33,6 +30,34 @@ def register():
     if not result.status:
         return result.message, 400
     return result.message, 200
+
+
+# The routes below are not implemented yet
+# They are there as placeholders
+
+# @app.route("/login_success/<name>")
+# def login_success(name: str):
+#    """Login succesful"""
+#    return f"welcome {name}"
+
+
+# @app.route("/login_failure/<name>")
+# def login_failure():
+#    """Login failure"""
+#    return "User not found, please try again"
+
+
+# @app.route("/register_success/<name>")
+# def register_success(name: str):
+#    """Resgiter successful"""
+#    return f"Register successful, welcome {name}"
+
+
+# @app.route("/register_failure/<name>")
+# def register_failure():
+#    """Register failure"""
+#    return "Register failed due to incomplete information, please try again"
+
 
 if __name__ == "__main__":
     app.run(debug=True)  # pragma: no cover
