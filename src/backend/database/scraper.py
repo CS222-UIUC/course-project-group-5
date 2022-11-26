635477e020d0270a0ef28a3b8edbfb602f50eec3
xxxfzxxx <30329924+xxxfzxxx@users.noreply.github.com>
2022-11-26 10:19:01 -0600

    Add files via upload

--- src/backend/database/scraper.py
+++ src/backend/database/scraper.py
@@ -2,30 +2,36 @@
 import re
 import sqlite3
 from bs4 import BeautifulSoup
+from dataclasses import dataclass
 
 con = sqlite3.connect("database_prod.db")
 
-"""I am a scraper"""
+
+@dataclass
+class AptInfo:
+    """Apt class, stores detail about an apartment"""
+
+    name: str
+    url: str
+    addr: str
+    price_range: str
 
 
 def scraper():
+    """I am a scraper"""
     with open("uiucApts.htm", "r", encoding="UTF-8") as file:
-        content = file.read()
-        soup = BeautifulSoup(content, features="html.parser")
+        soup = BeautifulSoup(file.read(), features="html.parser")
         apts = soup.find_all("div", class_="c-list")
         for i, apt in enumerate(apts):
             mobile_header = apt.find("div", class_="mobileHeader")
             image_section = apt.find("div", class_="slick-track")
             campus_detail = apt.find("div", class_="campusDetail")
+            apt = read_apt(mobile_header, campus_detail)
 
-            apt_name = mobile_header.find("a").text
-            apt_url = mobile_header.find("a")["href"]
-            apt_addr = mobile_header.find("span", class_="ellipsis").text
-            price_range = campus_detail.find("em", class_="rent_style").text.strip()
-            apt_price_min, apt_price_max = format_price(price_range)
+            apt_price_min, apt_price_max = format_price(apt.price_range)
 
             write_to_apt_db(
-                i, [apt_name, apt_addr, apt_price_min, apt_price_max, apt_url]
+                i, [apt.name, apt.addr, apt_price_min, apt_price_max, apt.url]
             )
             if image_section:
                 imgs = image_section.findAll("img")
@@ -37,10 +43,17 @@ def scraper():
         con.commit()
 
 
-"""A helper function to format string"""
+def read_apt(header, detail):
+    """ A helper function to read apt in to apt dataclass"""
+    apt_name = header.find("a").text
+    apt_url = header.find("a")["href"]
+    apt_addr = header.find("span", class_="ellipsis").text
+    price_range = detail.find("em", class_="rent_style").text.strip()
+    return AptInfo(apt_name, apt_url, apt_addr, price_range)
 
 
 def format_price(price_range):
+    """A helper function to format string"""
     if re.search("[a-aA-Z]", price_range):
         return -1, -1  # price not avaiable
     res = re.findall(r"\d+", price_range)
@@ -51,10 +64,8 @@ def format_price(price_range):
     return [int(res[0]), int(res[1])]
 
 
-"""A helper function to execute INSERT into Apartments"""
-
-
 def write_to_apt_db(apt_id, info):
+    """A helper function to execute INSERT into Apartments"""
     cur = con.cursor()
     cur.execute(
         "INSERT INTO Apartments (apt_id, apt_name, apt_address, price_min, price_max, link) \
@@ -63,15 +74,13 @@ def write_to_apt_db(apt_id, info):
     )
 
 
-"""A helper function to execute INSERT into AptPics"""
-
-
-def write_to_img_db(id, url):
+def write_to_img_db(apt_id, url):
+    """A helper function to execute INSERT into AptPics"""
     cur = con.cursor()
     cur.execute(
         "INSERT INTO AptPics (apt_id, link) \
         VALUES (?, ?)",
-        (id, url),
+        (apt_id, url),
     )
 
 
